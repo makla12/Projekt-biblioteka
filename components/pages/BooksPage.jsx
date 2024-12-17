5d3836b5a937cb646f137d0a592080eb556e9bed
makla12 <144844324+makla12@users.noreply.github.com>
2024-12-17 19:23:04 +0100

    update

--- components/pages/BooksPage.jsx
+++ components/pages/BooksPage.jsx
@@ -1,12 +1,20 @@
 import NavBar from "../elements/NavBar";
 import Footer from "../elements/Footer";
+import BookLink from "../elements/BookLink";
 
 export default function BooksPage() {
   	return (
 	<>
         <NavBar curentSiteId={2} />
         
-		<div className="w-full h-96"> </div>
+		<div className="w-full h-auto px-12 py-5 flex justify-center gap-x-96 gap-y-20 flex-wrap">
+			<BookLink href={"/books/book1"} />
+			<BookLink href={"/books/book1"} />
+			<BookLink href={"/books/book1"} />
+			<BookLink href={"/books/book1"} />
+			<BookLink href={"/books/book1"} />
+			<BookLink href={"/books/book1"} />
+		</div>
 
 		<Footer />
     </>
