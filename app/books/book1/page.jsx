30429f55f68f6a7820431d9a24baed01228e55f0
makla12 <1234makla@gmail.com>
2025-01-13 11:38:03 +0100

    Update page.jsx

--- app/books/book1/page.jsx
+++ app/books/book1/page.jsx
@@ -1,37 +1,32 @@
 import BookInfo from "@/components/elements/BookInfo";
 
-export default function book(){
-    const bookObj = {
-        title: "NAZWA KSIĄŻKI",
-        author: "Gal anonim",
-        date: "2022",
-        type: "Akcja",
-        rating: "8/10",
-        about: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta neque sapiente accusamus, temporibus ipsam totam provident nisi sed dolores! Mollitia voluptate enim modi eum culpa! Tenetur cum maxime sit quod.",
-        reviews:[
-            { 
-                user:"Imię i nazwisko",
-                date:"12-02-2024",
-                rating:"5/10",
-                review: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor voluptatibus inventore ex aut obcaecati consequuntur cupiditate odio ea delectus a iure nihil nesciunt iusto, animi optio eveniet laboriosam quas itaque.",
-            },
+export default function book(){return (<BookInfo {...{
+    title: "NAZWA KSIĄŻKI",
+    author: "Gal anonim",
+    date: "2022",
+    type: "Akcja",
+    rating: "8/10",
+    about: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta neque sapiente accusamus, temporibus ipsam totam provident nisi sed dolores! Mollitia voluptate enim modi eum culpa! Tenetur cum maxime sit quod.",
+    reviews:[
+        { 
+            user:"Imię i nazwisko",
+            date:"12-02-2024",
+            rating:"5/10",
+            review: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor voluptatibus inventore ex aut obcaecati consequuntur cupiditate odio ea delectus a iure nihil nesciunt iusto, animi optio eveniet laboriosam quas itaque.",
+        },
 
-            { 
-                user:"",
-                date:"",
-                rating:"",
-                review:"",
-            },
+        { 
+            user:"",
+            date:"",
+            rating:"",
+            review:"",
+        },
 
-            { 
-                user:"",
-                date:"",
-                rating:"",
-                review:"",
-            },
-
-        ],
-    }
-
-    return <BookInfo {...bookObj} />;
-}
+        { 
+            user:"",
+            date:"",
+            rating:"",
+            review:"",
+        },
+    ],
+}}/>)};
