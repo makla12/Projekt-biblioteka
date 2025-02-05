af2aa83811133dfa4ce1c3fe8aad2f1a4d810b89
Timur <timurkn2006@gmail.com>
2025-02-05 17:54:21 +0100

    added books

--- components/pages/BooksPage.jsx
+++ components/pages/BooksPage.jsx
@@ -9,11 +9,11 @@ export default function BooksPage() {
         
 		<div className="w-full h-auto px-12 py-5 flex justify-center gap-x-96 gap-y-20 flex-wrap">
 			<BookLink href={"/books/book1"} />
-			<BookLink href={"/books/book1"} />
-			<BookLink href={"/books/book1"} />
-			<BookLink href={"/books/book1"} />
-			<BookLink href={"/books/book1"} />
-			<BookLink href={"/books/book1"} />
+			<BookLink href={"/books/book2"} />
+			<BookLink href={"/books/book3"} />
+			<BookLink href={"/books/book4"} />
+			<BookLink href={"/books/book5"} />
+			<BookLink href={"/books/book6"} />
 		</div>
 
 		<Footer />
