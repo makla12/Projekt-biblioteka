c75bbfebe63ff21c1127759888ecdfadc38b5763
makla <144844324+makla12@users.noreply.github.com>
2025-02-06 19:17:04 +0100

    chat icon

--- components/elements/ChatContainer.jsx
+++ components/elements/ChatContainer.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Image from "next/image";
+import chatIcon from "@/public/chat.svg"
 import Chat from "./Chat";
 
 export default function ChatContainer(){
@@ -13,14 +15,15 @@ export default function ChatContainer(){
             "
             onClick={()=>{setIsShown(true)}}
         >
-            <div className="w-3/4 h-3/4 bg-blue-500 rounded-full animate-ping" />
+            <div className="w-3/4 h-3/4 bg-blue-500 rounded-full animate-ping absolute" />
+            <Image src={chatIcon} alt="" className="w-1/2 h-1/2" />
         </div>
         
         <div className={`
-            w-full h-full sm:w-80 ${"sm:aspect-[3/5]" /* NIE DZIAŁA */} fixed right-0 top-0 sm:top-[10%] flex flex-col items-end p-1
+            w-full h-full sm:w-[30rem] sm:h-[55rem] fixed right-0 top-0 sm:top-[10%] flex flex-col items-end p-1
             sm:rounded-lg bg-blue-500 transition-transform duration-500 ${!isShown ? "translate-x-full" : "translate-x-0"}`}
         >
-            <div className="w-10 aspect-square bg-red-500 rounded-lg right-1 top-1 cursor-pointer" onClick={()=>{setIsShown(false)}}></div>
+            <div className="w-10 aspect-square bg-red-500 rounded-lg right-1 top-1 cursor-pointer flex justify-center items-center font-bold text-xl" onClick={()=>{setIsShown(false)}}>X</div>
             <Chat />
         </div>
     </>
