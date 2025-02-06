c4ea00d6114d2712d80b43da430380971fc5060f
makla <144844324+makla12@users.noreply.github.com>
2025-02-06 22:25:43 +0100

    chat backdrop blur

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
 	<html lang="en">
 		<body
-			className={`antialiased`}
+			className={`antialiased min-h-[100vh]`}
 		>
 			{children}
 		</body>
