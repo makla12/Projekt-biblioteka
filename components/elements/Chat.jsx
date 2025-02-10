5a4c142b41fa71d43827c4026977c2beb0c2b29b
makla12 <1234makla@gmail.com>
2025-02-10 11:21:15 +0100

    cat change

--- components/elements/Chat.jsx
+++ components/elements/Chat.jsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+import sendIcon from "@/public/send.svg";
+
 export default function Chat() {
     return (
     <>
-        <div className="w-full h-full">
-
+        <div className="w-full h-full felx flex-col-reverse justify-end">
+            <form onSubmit={(e)=>{e.preventDefault()}}>
+                <div className="w-full flex gap-2">
+                    <input type="text" className="flex-grow bg-background rounded-md p-1" />
+                    <button className="w-8 p-1 rounded-md aspect-square bg-background flex justify-center items-center">
+                        <Image src={sendIcon} alt="" className="w-full h-full dark:invert" />
+                    </button>
+                </div>
+            </form>
         </div>
     </>
     );
--- components/elements/ChatContainer.jsx
+++ components/elements/ChatContainer.jsx
@@ -16,16 +16,16 @@ export default function ChatContainer(){
             onClick={()=>setIsShown(true)}
         >
             <div className="w-3/4 h-3/4 bg-blue-500 rounded-full animate-ping absolute" />
-            <Image src={chatIcon} alt="" className="w-1/2 h-1/2" />
+            <Image src={chatIcon} alt="" className="w-1/2 h-1/2 dark:invert" />
         </div>
 
         <div onClick={()=>setIsShown(false)} className={`w-full h-[100vh] backdrop-blur-sm fixed right-0 top-0 ${!isShown ? "translate-x-full" : "translate-x-0"}`}></div>
         
         <div className={`
             w-full h-full sm:w-[30rem] fixed right-0 top-0 flex flex-col items-end p-1
-            sm:rounded-s-lg bg-blue-500 transition-transform duration-500 ${!isShown ? "translate-x-full" : "translate-x-0"}`}
+            sm:rounded-s-lg bg-blue-700 transition-transform duration-500 ${!isShown ? "translate-x-full" : "translate-x-0"}`}
         >
-            <div className="w-10 aspect-square bg-red-500 rounded-lg right-1 top-1 cursor-pointer flex justify-center items-center font-bold text-xl" onClick={()=>setIsShown(false)}>X</div>
+            <div className="w-10 aspect-square bg-red-700 rounded-lg right-1 top-1 cursor-pointer flex justify-center items-center font-bold text-xl" onClick={()=>setIsShown(false)}>X</div>
             <Chat />
         </div>
     </>
