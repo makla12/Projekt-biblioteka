957de6f79997953ea4f170d0e87d6e383cb42daf
Timur <93942023+pr1xt@users.noreply.github.com>
2025-05-19 14:48:36 +0200

    img update

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import ChatContainer from "../elements/chat/ChatContainer";
 import NavBar from "../elements/NavBar";
 import Footer from "../elements/Footer";
+import Image from "next/image";
+import Illustration from "@/public/book_power.png"
 
 export default function HomePage() {
 	return (
@@ -8,7 +10,9 @@ export default function HomePage() {
 		<ChatContainer />
         <NavBar curentSiteId={0} />
         
-		<div className="w-full h-96"> </div>
+		<div className="w-full h-96"> 
+			<Image src={Illustration} alt="Book Power Image" className="w-[99%] sm:w-3/4 m-auto" ></Image>
+		</div>
 
         <Footer />
     </>
