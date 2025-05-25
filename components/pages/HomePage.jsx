4a213b45a3e1898cc5f9c23d79c46ee362968c5b
Timur <93942023+pr1xt@users.noreply.github.com>
2025-05-25 20:03:36 +0200

    dark n light image update

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -2,7 +2,8 @@ import ChatContainer from "../elements/chat/ChatContainer";
 import NavBar from "../elements/NavBar";
 import Footer from "../elements/Footer";
 import Image from "next/image";
-import Illustration from "@/public/book_power.png"
+import IllustrationDark from "@/public/book_power.png";
+import IllustrationLight from "@/public/book_power_light.png";
 
 export default function HomePage() {
 	return (
@@ -10,9 +11,21 @@ export default function HomePage() {
 		<ChatContainer />
         <NavBar curentSiteId={0} />
         
-		<div className="w-full h-96"> 
-			<Image src={Illustration} alt="Book Power Image" className="w-[99%] sm:w-3/4 m-auto" ></Image>
-		</div>
+		<div className="w-full h-96 ">
+                {/* Light mode image */}
+                <div className="block dark:hidden w-[99%] sm:w-3/4 m-auto">
+                    <Image
+                        src={IllustrationLight} alt="Book Power"
+                    />
+                </div>
+
+                {/* Dark mode image */}
+                <div className="hidden dark:block w-[99%] sm:w-3/4 m-auto">
+                    <Image
+                        src={IllustrationDark} alt="Book Power"
+                    />
+                </div>
+            </div>
 
         <Footer />
     </>
