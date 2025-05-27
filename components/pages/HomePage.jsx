fff21ef4027c49d4b10dd6ac3246d6ac8223d086
Timur <93942023+pr1xt@users.noreply.github.com>
2025-05-27 21:56:49 +0200

    MainPageLittleUpdate

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -11,18 +11,54 @@ export default function HomePage() {
 		<ChatContainer />
         <NavBar curentSiteId={0} />
         
-		<div className="w-full h-96 ">
-                <div className="block dark:hidden w-[99%] sm:w-3/4 m-auto"> {/*light mode*/}
-                    <Image
-                        src={IllustrationLight} alt="Book Power"
-                    />
-                </div>
-                <div className="hidden dark:block w-[99%] sm:w-3/4 m-auto">  {/*dark mode*/}
-                    <Image
-                        src={IllustrationDark} alt="Book Power"
-                    />
-                </div>
+		<div className="w-full h-auto pb-5 pt-7 ">
+            <div className="block dark:hidden sm:w-3/4 m-auto "> {/*light mode*/}
+                <Image className="w-full"
+                    src={IllustrationLight} alt="Book Power"
+                />
             </div>
+            <div className="hidden dark:block sm:w-3/4 m-auto ">  {/*dark mode*/}
+                <Image className="w-full"
+                    src={IllustrationDark} alt="Book Power"
+                />
+            </div>
+        </div>
+        <div className="w-full h-auto bg-blue-800  text-center py-7">
+            <h1 className="w-full text-5xl font-bold ">
+                Biblioteka z Energią!
+            </h1>
+        </div>
+        <div className="h-[50vh] flex justify-between flex-row  py-6">
+            <div className="bg-[var(--background2)] mx-auto w-[40vw] h-100% rounded-xl p-9 text-[1.5vw]">
+                <p>
+                    <b>Decydujesz, co przeczytać w następnej kolejności?</b><br></br><br></br>
+                    Jesteś we właściwym miejscu. Powiedz nam, jakie tytuły 
+                    lub gatunki podobały ci się w przeszłości, a my przedstawimy 
+                    ci zaskakująco wnikliwe rekomendacje.
+                </p>
+            </div>
+            <div className="bg-[var(--background2)] mx-auto w-[40vw] h-100% rounded-xl p-9 text-[1.5vw]">
+                
+                    <h2><b>Zapraszamy do biblioteki!</b></h2>
+                    <span >Szukasz spokojnego miejsca, gdzie możesz popracować, pouczyć się albo spędzić czas z kolegami? Biblioteka to idealne miejsce</span><br></br>
+                    <span>Możesz przyjść sam albo z grupą znajomych. Oferujemy:</span>
+                    {/* <ul className="list-disc px-4 ">
+
+                        <li>ciche strefy do nauki i pracy</li>
+
+                        <li>dostęp do komputerów i internetu</li>
+
+                        <li>gry planszowe, przy których można się zrelaksować i miło spędzić czas</li>
+
+                        <li>przyjazną atmosferę sprzyjającą zarówno skupieniu, jak i rozmowie</li>
+                    </ul> */}
+
+                    <span>Nie trzeba się zapisywać — wystarczy przyjść.
+                    Do zobaczenia w bibliotec</span>
+               
+            </div>
+           
+        </div>
 
         <Footer />
     </>
