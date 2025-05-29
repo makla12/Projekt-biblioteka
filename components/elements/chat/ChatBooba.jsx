63f04434b2574c0e5da395282076b2e2569efc5a
Timur <93942023+pr1xt@users.noreply.github.com>
2025-05-29 13:24:18 +0200

    chatfix

--- components/elements/chat/ChatBooba.jsx
+++ components/elements/chat/ChatBooba.jsx
@@ -1,7 +1,7 @@
 export default function ChatBooba({ message, self }){
     return(
     <>
-        <div className={`w-fit max-w-64 h-auto ${self ? "bg-blue-400 dark:bg-blue-500 self-end" : "bg-slate-300 dark:bg-slate-800"} py-2 px-4 rounded-3xl text-lg`}>{message}</div>
+        <div className={`w-fit max-w-64 rounded-3xl  h-auto ${self ? "bg-blue-400 dark:bg-blue-500 self-end rounded-br-sm" : "bg-slate-300 dark:bg-slate-800 rounded-bl-sm"} py-2 px-4  text-lg`}>{message}</div>
     </>
     )
 }
