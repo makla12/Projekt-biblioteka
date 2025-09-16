62b76f2ffcd3367e27d78a34be99301575233905
makla <144844324+makla12@users.noreply.github.com>
2025-09-16 11:24:55 +0200

    Update fictional paragraf footer

--- components/elements/Footer.jsx
+++ components/elements/Footer.jsx
@@ -1,7 +1,13 @@
 export default function Footer(){
     return(
     <>
-        <div className="w-full h-20 bg-[var(--background2)]"></div>
+        <div className="w-full h-auto p-4 bg-[var(--background2)] flex justify-center items-center">
+            <p className="text-sm text-zinc-400 text-center">
+                Wszystkie postacie, korporacje, instytucje i informacje pojawiające się w tej pracy są fikcyjne. 
+                Wszelkie podobieństwo do prawdziwych firm lub osób jest czysto przypadkowe. 
+                Nie należy kontaktować się z innymi firmami o podobnych nazwach.
+            </p>
+        </div>
     </>
     );
 }
--- components/elements/chat/dialogs/endDialog.js
+++ components/elements/chat/dialogs/endDialog.js
@@ -3,6 +3,7 @@ import { message, goToDialogPath, checkInventory } from "./dialogFunctions";
 const endMessages = [
     () => checkInventory(["kawalek_karty_307", "kawalek_karty_214", "kawalek_karty_101"],[
                 () => message("To już ostatni kawałek karty w końcu mogę się stąd wydostać"),
+                () => message("Złuchaście na lekcjach pana suchojada bo inaczej byście nie wiedzieli jak to zrobić"),
                 () => message("Bardzo dziękuje za twoją pomoc"),
                 () => message("(TEMP) (można napisać tutaj coś o nagrodzie)"),
                 () => goToDialogPath("/endDialog"),
